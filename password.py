bb673d62d49be939e770c4bb2d0c0ff1fc29dcdb
thecsmine <thecsmine@gmail.com>
2020-12-12 17:50:34 +0100

    Delete password.py

--- password.py
+++
@@ -1,9 +0,0 @@
-class Password:
-    def __init__(self, parent_account = None, hash = None, crypted = None):
-        if hash:
-            self.hash = hash
-        elif crypted:
-            self.crypted = crypted
-
-        if parent_account:
-            self.parent_account = parent_account
