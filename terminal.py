9535119dac3a6281ce0ba6fdfff3f516c670e14f
thecsmine <thecsmine@gmail.com>
2020-12-24 12:28:07 +0100

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -6,3 +6,12 @@ def clear():
         return os.system('cls')
     else:
         return os.system('clear')
+
+def get_all_files_from(folder="data/account"):
+    """ Folder should be a string like "data/category" or "data/account"
+    """
+    l = []
+    for path, subdirs, files in os.walk(folder):
+        for name in files:
+            l.append(os.path.join(path, name)[len(folder)+1:].replace("\\", "/"))
+    return l
