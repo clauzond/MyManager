33d4e5329a7b07c25950b26f03fe72cccaf1f744
thecsmine <thecsmine@gmail.com>
2020-12-29 01:15:39 +0100

    Launch app

---
+++ main.py
@@ -0,0 +1,19 @@
+import pip
+
+
+def import_necessary():
+    __import__("Crypto.Cipher")
+    __import__("hashlib")
+    __import__("pyperclip")
+    __import__("termcolor")
+
+
+if __name__ == "__main__":
+    try:
+        import menu
+        import_necessary()
+    except (ImportError, ModuleNotFoundError):
+        pip.main(["install", "-r", "requirements.txt"])
+
+    import menu
+    menu.main_menu()
